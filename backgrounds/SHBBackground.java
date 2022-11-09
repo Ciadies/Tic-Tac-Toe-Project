05dd65694f8e1b119233465ecb5db59112673c30
1005333001 <1005333001@L829WD210023.edu.cbe.ab.ca>
2022-11-09 13:39:49 -0700

    added background file

---
+++ backgrounds/SHBBackground.java
@@ -0,0 +1,53 @@
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+public class SHBBackground implements Background{
+
+	@Override
+	public Tile getTile(int col, int row) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getCol(double x) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRow(double y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getShiftX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getShiftY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setShiftX(double shiftX) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setShiftY(double shiftY) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
